1e057a9c473cff8b5a0d3275d9740158ba0aa32c
duncan982 <ndegwaduncan@icloud.com>
2022-04-10 11:54:16 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,53 @@
+// server.js
+// where your node app starts
+
+// init project
+require('dotenv').config();
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+//  get ip address and borwser details
+
+app.get("/api/:whoami?", (req, res) => {
+  // ipAddress = req.params.whoami
+  // console.log(ipAddress)
+
+  var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
+
+  var browserLanguage = req.headers["accept-language"];
+
+  var browserAgent = req.headers["user-agent"]
+
+  // console.log(ip)
+  res.json({
+    ipaddress: ip,
+    language: browserLanguage,
+    software: browserAgent
+});
+
+});
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
